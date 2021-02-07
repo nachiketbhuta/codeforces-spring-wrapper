c394f75ca66570d30a9ce5a606d88f0e43d43349
Nachiket <nachiketbhuta@gmail.com>
2021-02-07 19:53:57 +0530

    testing setup done

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -3,7 +3,9 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.example.codeforces.CodeforcesApplication;
+
+@SpringBootTest(classes = CodeforcesApplication.class)
 class DemoApplicationTests {
 
 	@Test
